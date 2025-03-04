3165ef76dc0bb712450c8a1d1324d022b1b26fc0
huang1shao <102003137+huang1shao@users.noreply.github.com>
2025-03-04 09:50:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 ./scripts/feeds install -af
+git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
